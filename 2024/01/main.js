30ba0a2806a1fc9fdb0c8d75529427033cd8f5d0
Giovani Rubim <giovani.rubim@brickabode.com>
2025-05-30 19:42:16 -0300

    Add solution for part 2 of day 1

--- 2024/01/main.js
+++ 2024/01/main.js
@@ -10,8 +10,16 @@ const input = require('fs')
 const array1 = input.map((row) => row[0]).sort()
 const array2 = input.map((row) => row[1]).sort()
 
-const sum = array1
-	.map((val, i) => Math.abs(val - array2[i]))
-	.reduce((a, b) => a + b, 0)
+const sum = array1.map((val, i) => Math.abs(val - array2[i])).reduce((a, b) => a + b, 0)
 
-console.log(sum)
+const countMap = {}
+for (const val of array2) {
+	countMap[val] = (countMap[val] ?? 0) + 1
+}
+let score = 0
+for (const val of array1) {
+	score += val * (countMap[val] ?? 0)
+}
+
+console.log('Part 1:', sum)
+console.log('Part 2:', score)
