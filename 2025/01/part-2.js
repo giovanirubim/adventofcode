c557e0cafeda94b4e70d75c8c256e4402eb22681
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2025-12-01 11:23:48 -0300

    Optimized Day 1 part 2

--- 2025/01/part-2.js
+++ 2025/01/part-2.js
@@ -5,16 +5,18 @@ const lines = inputText.trim().split('\n');
 
 let pointer = 50;
 let res = 0;
-const incMap = { R: 1, L: 99 };
 
 for (const line of lines) {
-	const dir = line[0];
 	let offset = Number(line.slice(1));
-	const inc = incMap[dir];
-	while (offset !== 0) {
-		pointer = (pointer + inc) % 100;
-		res += pointer === 0;
-		offset -= 1;
+	const R = line[0] === 'R';
+	const sign = R ? 1 : -1;
+	const toReach0 = R ? 100 - pointer : pointer || 100;
+	if (offset < toReach0) {
+		pointer = (pointer + offset * sign + 100) % 100;
+	} else {
+		offset -= toReach0;
+		res += 1 + Math.floor(offset / 100);
+		pointer = ((offset % 100) * sign + 100) % 100;
 	}
 }
 
