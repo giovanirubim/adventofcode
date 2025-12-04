35836e2fef577fc85f8bfe72c5eec7c1303ab24c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2025-12-04 09:23:04 -0300

    Simplify 2025/04 part 2

--- 2025/04/part-2.js
+++ 2025/04/part-2.js
@@ -5,18 +5,6 @@ const solve = (text) => {
 	const grid = lines.map((l) => l.split(''));
 	const n = grid.length;
 	const m = grid[0].length;
-	const getNeighbors = (i, j) => {
-		const res = [];
-		for (let di = -1; di <= 1; di++) {
-			for (let dj = -1; dj <= 1; dj++) {
-				if (di === 0 && dj === 0) continue;
-				if (grid[i + di]?.[j + dj] === '@') {
-					res.push([i + di, j + dj]);
-				}
-			}
-		}
-		return res;
-	};
 	const checkList = [];
 	const isInCheckList = {};
 	const add = (i, j) => {
@@ -32,7 +20,6 @@ const solve = (text) => {
 		isInCheckList[key] = false;
 		return item;
 	};
-	const isRemovable = (i, j) => getNeighbors(i, j).length < 4;
 	for (let i = 0; i < n; i++) {
 		for (let j = 0; j < m; j++) {
 			if (grid[i][j] !== '@') continue;
@@ -42,10 +29,19 @@ const solve = (text) => {
 	let res = 0;
 	while (checkList.length !== 0) {
 		const [i, j] = pop();
-		if (!isRemovable(i, j)) continue;
+		const neighbors = [];
+		for (let di = -1; di <= 1; di++) {
+			for (let dj = -1; dj <= 1; dj++) {
+				if (di === 0 && dj === 0) continue;
+				if (grid[i + di]?.[j + dj] === '@') {
+					neighbors.push([i + di, j + dj]);
+				}
+			}
+		}
+		if (neighbors.length >= 4) continue;
 		grid[i][j] = '.';
 		res += 1;
-		getNeighbors(i, j).forEach(([i, j]) => add(i, j));
+		for (const [i, j] of neighbors) add(i, j);
 	}
 	return res;
 };
