3e46767bafaa1e4c66a8e89f092bc91821f12ef0
Giovani Rubim <giovanirubim.f@gmail.com>
2025-12-10 23:50:13 -0300

    Add partial solutions for 2025/10

--- 2025/10/part-1.js
+++ 2025/10/part-1.js
@@ -1,13 +1,59 @@
 import fs from 'fs';
 
+const parseMachine = (machine) => {
+	const [a, b] = machine.split(/(?<=])\s|\s(?={)/);
+	const bitMap = { '#': 1, '.': 0 };
+	const bits = a.split('').map((c) => bitMap[c] ?? '');
+	const target = parseInt(bits.reverse().join(''), 2);
+	const buttons = b
+		.replace(/[()]/g, '')
+		.split(' ')
+		.map((s) =>
+			s
+				.split(',')
+				.map((i) => 1 << i)
+				.reduce((a, b) => a + b, 0)
+		);
+	return { target, buttons };
+};
+
+const solveMachine = ({ target, buttons }) => {
+	if (target === 0) return 0;
+	let front = [0];
+	const visited = new Set([0]);
+	let presses = 0;
+	while (front.length !== 0) {
+		presses += 1;
+		const add = [];
+		for (const stt of front) {
+			for (const b of buttons) {
+				const nextStt = stt ^ b;
+				if (nextStt === target) return presses;
+				if (visited.has(nextStt)) continue;
+				visited.add(nextStt);
+				add.push(nextStt);
+			}
+		}
+		front = add;
+	}
+	return presses;
+};
+
 const solve = (text) => {
-	return 1;
+	const lines = text.trim().split('\n');
+	let res = 0;
+	for (const line of lines) {
+		res += solveMachine(parseMachine(line));
+	}
+	return res;
 };
 
 const sampleText = `
-sample text here
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
 `;
-const expected = 0;
+const expected = 7;
 const actual = solve(sampleText);
 
 if (actual !== expected) {
