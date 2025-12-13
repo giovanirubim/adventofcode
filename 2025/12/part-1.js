1da293a725304cbd6c1a0b202679f75e345a2ff4
Giovani Rubim <giovanirubim.f@gmail.com>
2025-12-12 22:00:25 -0300

    Add solution for 2025/12 part 1

--- 2025/12/part-1.js
+++ 2025/12/part-1.js
@@ -1,23 +1,27 @@
 import fs from 'fs';
 
 const solve = (text) => {
-	return 1;
+	const lines = text.trim().split('\n');
+	const shapes = [];
+	let res = 0;
+	for (const line of lines) {
+		if (!line.trim()) continue;
+		if (line.includes(':') && line.includes('x')) {
+			const [a, b] = line.split(':');
+			const [width, height] = a.trim().split('x').map(Number);
+			const area = width * height;
+			const counts = b.trim().split(' ').map(Number);
+			const sum = counts.map((count, i) => count * shapes[i]).reduce((a, b) => a + b, 0);
+			res += sum < area;
+		} else if (line.includes(':')) {
+			shapes.push(0);
+		} else {
+			const [a, b, c] = line.split('').map((c) => '.#'.indexOf(c));
+			shapes[shapes.length - 1] += a + b + c;
+		}
+	}
+	return res;
 };
-
-const sampleText = `
-sample text here
-`;
-const expected = 0;
-const actual = solve(sampleText);
-
-if (actual !== expected) {
-	console.error('Failed with sample input');
-	console.error('Expected:', expected);
-	console.error('Actual:  ', actual);
-	process.exit(1);
-}
-
-console.log('Matched sample result');
 
 const inputText = fs.readFileSync('./input.txt', 'ascii');
 const start = performance.now();
