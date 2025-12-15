3efe930ea72833ff0663016e57091e7c8743a41d
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2025-12-15 18:27:34 -0300

    Update 2025/10 part 2 (still WIP)

--- 2025/10/part-2-WIP.js
+++ 2025/10/part-2-WIP.js
@@ -2,85 +2,104 @@ import fs from 'fs';
 
 const hasOneBit = (x) => (x & (x - 1)) === 0;
 const lowestBit = (x) => (x & (x - 1)) ^ x;
+const maxMin = (a, b) => [Math.max(a, b), Math.min(a, b)];
+const numArr = (str) => str.split(',').map(Number);
 
-const prune = (tree, srcMap) => {
-	const prunedTree = {};
-	const prunedSrc = {};
-	const copy = (mask) => {
-		if (prunedTree[mask] !== undefined) return;
-		const val = tree[mask];
-		prunedTree[mask] = val;
-		if (srcMap[mask] !== undefined) {
-			prunedSrc[mask] = srcMap[mask];
-		}
-		if (val !== null) {
-			const [a, b] = val;
-			copy(a);
-			copy(b);
-		}
-	};
-	let mask = 1;
-	while (tree[mask] !== undefined) {
-		copy(mask);
-		mask <<= 1;
-	}
-	return [prunedTree, prunedSrc];
-};
-
-const buildMasksBySrcMap = (srcMap) => {
+const pruneTree = (tree, nBits) => {
 	const res = {};
-	for (const sMask in srcMap) {
-		const mask = Number(sMask);
-		const val = srcMap[mask];
-		let keys = res[val];
-		if (keys === undefined) {
-			keys = [];
-			res[val] = keys;
-		}
-		keys.push(mask);
+	const copy = (mask) => {
+		const val = (res[mask] = tree[mask]);
+		val?.map(copy);
+	};
+	for (let i = 0; i < nBits; i++) {
+		copy(1 << i);
 	}
+	const all = (1 << nBits) - 1;
+	copy(all);
 	return res;
 };
 
-const getInferenceTree = (masks, nBits) => {
-	let tree = {};
-	let srcMap = {};
-	const all = (1 << nBits) - 1;
-	const missing = [];
-	let unknown = all;
-	for (const mask of masks) {
-		tree[mask] = null;
-		if (hasOneBit(mask)) {
-			unknown ^= mask;
-		}
+const pruneDepMap = (depMap, tree) => {
+	const map = {};
+	for (const sMask in tree) {
+		map[sMask] = depMap[sMask];
 	}
-	let src = null;
-	for (let i = 0; unknown !== 0; i++) {
-		const mi = masks[i];
-		for (let j = 0; j < i && unknown !== 0; j++) {
-			const mj = masks[j];
-			const [major, minor] = mi > mj ? [mi, mj] : [mj, mi];
+	return map;
+};
+
+const remapDepMap = (depMap, tree) => {
+	const map = {};
+	for (const sMask in tree) {
+		const dep = depMap[sMask];
+		if (dep === null) continue;
+		map[dep] = [];
+	}
+	for (const sMask in tree) {
+		const dep = depMap[sMask];
+		if (dep === null) continue;
+		const mask = Number(sMask);
+		map[dep].push(mask);
+	}
+	return map;
+};
+
+const cascade = (system, rootMask, affectedMasks, tree, depMap) => {
+	const memo = {};
+	let used = 0;
+	const solve = (mask) => {
+		if (depMap[mask] !== rootMask) return system[mask];
+		if (memo[mask] !== undefined) return memo[mask];
+		const [major, minor] = tree[mask];
+		const res = solve(major) - solve(minor);
+		if (hasOneBit(mask)) used += res;
+		return (system[mask] = memo[mask] = res);
+	};
+	for (const mask of affectedMasks) {
+		solve(mask);
+	}
+	return used;
+};
+
+const mapSystemInferences = (masks, nBits) => {
+	const all = (1 << nBits) - 1;
+	let unknown = all;
+	let tree = {};
+	let depMap = {};
+	for (const mask of masks) {
+		tree[mask] = depMap[mask] = null;
+		if (hasOneBit(mask)) unknown ^= mask;
+	}
+	const missing = [];
+	let dep = null;
+	for (let i = 1; i < masks.length; i++) {
+		const iMask = masks[i];
+		for (let j = 0; j < i; j++) {
+			const jMask = masks[j];
+			const [major, minor] = maxMin(iMask, jMask);
 			if ((major & minor) !== minor) continue;
 			const mask = major ^ minor;
 			if (tree[mask] !== undefined) continue;
+			masks.push(mask);
 			tree[mask] = [major, minor];
-			masks.push(mask);
+			depMap[mask] = dep;
 			if (hasOneBit(mask)) unknown ^= mask;
-			if (src !== null) srcMap[mask] = src;
 		}
-		if (i + 1 === masks.length) {
-			if (unknown === 0) break;
-			const mask = missing.length === 0 ? all : lowestBit(unknown);
-			if (mask !== all) unknown ^= mask;
-			masks.push(mask);
-			missing.push(mask);
-			src = mask;
-			tree[mask] = null;
+		if (unknown === 0) break;
+		if (i === masks.length - 1) {
+			let newMask = all;
+			if (missing.length !== 0) {
+				newMask = lowestBit(unknown);
+				unknown ^= newMask;
+			}
+			masks.push(newMask);
+			tree[newMask] = depMap[newMask] = null;
+			missing.push(newMask);
+			dep = newMask;
 		}
 	}
-	[tree, srcMap] = prune(tree, srcMap);
-	const masksBySrc = buildMasksBySrcMap(srcMap);
-	return { missing, tree, masksBySrc };
+	tree = pruneTree(tree, nBits);
+	depMap = pruneDepMap(depMap, tree);
+	return { tree, missing, depMap };
 };
 
 const solveMask = (mask, system, tree, memo) => {
@@ -93,84 +112,140 @@ const solveMask = (mask, system, tree, memo) => {
 	return dfs(mask);
 };
 
-const runNegChecks = (masks, system, tree, memo) => {
+const hasNegative = (system, masks) => {
 	for (const mask of masks) {
-		if (solveMask(mask, system, tree, memo) < 0) return false;
+		if (system[mask] < 0) return true;
 	}
-	return true;
+	return false;
 };
 
-const inputText = fs.readFileSync('./input.txt', 'ascii');
-const lines = inputText.trim().split('\n');
-
-const solveCompleteSystem = (system, nBits, tree) => {
-	const memo = {};
-	const counts = Array(nBits);
+const computeSolution = (system, nBits, tree, memo) => {
+	let sum = 0;
 	for (let i = 0; i < nBits; i++) {
 		const mask = 1 << i;
-		counts[i] = solveMask(mask, system, tree, memo);
+		sum += solveMask(mask, system, tree, memo);
 	}
-	return counts;
+	return sum;
 };
 
-const validateSolution = (system, counts) => {
-	const sumMap = {};
-	for (let i = 0; i < counts.length; i++) {
-		const sum = counts[i];
-		if (sum < 0) return false;
-		sumMap[1 << i] = sum;
-	}
+const validSystem = (system) => {
 	for (const sMask in system) {
 		let mask = Number(sMask);
 		if (hasOneBit(mask)) continue;
-		let sum = system[mask];
+		let sum = 0;
 		while (mask !== 0) {
 			const bit = lowestBit(mask);
-			sum -= sumMap[bit];
 			mask ^= bit;
+			sum += system[bit];
 		}
-		if (sum !== 0) return false;
+		if (sum !== system[sMask]) {
+			return false;
+		}
 	}
 	return true;
 };
 
-lines.forEach((line, i) => {
+const preCalc = (system, tree, depMap, ignore) => {
+	let sum = 0;
+	for (const sMask in tree) {
+		if (depMap[sMask] !== null) continue;
+		if (ignore[sMask] !== undefined) continue;
+		solveMask(Number(sMask), system, tree, system);
+		const mask = Number(sMask);
+		if (hasOneBit(mask)) sum += system[sMask];
+	}
+	return sum;
+};
+
+const solveLine = (line) => {
 	const clear = line.replace(/[^\d,{\s]/g, '').trim();
 	const [a, b] = clear.split(' {');
-	const vals = b.split(',').map(Number);
-	const buttons = a.split(' ').map((b) => b.split(',').map(Number));
+	const vals = numArr(b);
+	const buttons = a.split(' ').map(numArr);
 	const system = {};
-	vals.forEach((val, vi) => {
-		let mask = 0;
-		buttons.forEach((b, bi) => {
-			if (b.includes(vi)) mask |= 1 << bi;
-		});
-		system[mask] = val;
-	});
 	const nBits = buttons.length;
+	const masks = Array(vals.length);
+	for (let vi = 0; vi < vals.length; vi++) {
+		let mask = 0;
+		for (let bi = 0; bi < nBits; bi++) {
+			mask |= buttons[bi].includes(vi) << bi;
+		}
+		if (system[mask] !== undefined) continue;
+		system[mask] = vals[vi];
+		masks[vi] = mask;
+	}
+	const { tree, missing, depMap } = mapSystemInferences(masks.slice(), nBits);
+	const ansMin = Math.max(...vals);
+	const ansMax = vals.reduce((a, b) => a + b);
 	const all = (1 << nBits) - 1;
-	const masks = Object.keys(system).map(Number);
-	const { missing, tree, masksBySrc } = getInferenceTree(masks.slice(), nBits);
 	if (missing.length === 0) {
-		const counts = solveCompleteSystem(system, nBits, tree);
-		const res = counts.reduce((a, b) => a + b, 0);
-		console.log(`Line ${i}:`, res, 'solved directly');
-	} else if (missing.length === 1) {
-		const min = Math.max(...vals);
-		const max = vals.reduce((a, b) => a + b, 0);
-		let res;
-		for (let total = min; total <= max; total++) {
-			system[all] = total;
-			const map = solveCompleteSystem(system, nBits, tree);
-			if (!runNegChecks(masksBySrc[all], system, tree, {})) {
-				continue;
-			}
-			if (validateSolution(system, map)) {
-				res = map.reduce((a, b) => a + b, 0);
-				break;
+		return computeSolution(system, nBits, tree, {});
+	}
+	const masksByDep = remapDepMap(depMap, tree);
+	const ignore = {};
+	for (const mask of missing) {
+		ignore[mask] = true;
+	}
+	let used = preCalc(system, tree, depMap, ignore);
+	if (missing.length === 1) {
+		const fx = masksByDep[all];
+		for (let ans = ansMin; ans <= ansMax; ans++) {
+			system[all] = ans;
+			cascade(system, all, fx, tree, depMap);
+			if (validSystem(system) && !hasNegative(system, fx)) {
+				return ans;
 			}
 		}
-		console.log(`Line ${i}:`, res, 'solved with simple search');
-	} else {
+		return NaN;
 	}
-});
+	const maskArr = missing.slice(1);
+	const maxArr = maskArr.map((mask) => {
+		let max = Infinity;
+		for (const sMask in system) {
+			if ((sMask & mask) === 0) continue;
+			if (system[sMask] === null) continue;
+			max = Math.min(max, system[sMask]);
+		}
+		return max;
+	});
+	const n = maskArr.length;
+	const bruteForce = (index, unused) => {
+		if (index === n) return validSystem(system);
+		const mask = maskArr[index];
+		const fx = masksByDep[mask] ?? [];
+		const max = Math.min(unused, maxArr[index]);
+		for (let i = 0; i < max; i++) {
+			system[mask] = i;
+			const used = i + cascade(system, mask, fx, tree, depMap);
+			if (hasNegative(system, fx)) continue;
+			if (bruteForce(index + 1, unused - used)) return true;
+		}
+		return false;
+	};
+	const fx = masksByDep[all] ?? [];
+	for (let ans = ansMin; ans <= ansMax; ans++) {
+		system[all] = ans;
+		const newUsed = used + cascade(system, all, fx, tree, depMap);
+		if (hasNegative(system, fx)) continue;
+		if (bruteForce(0, ans - newUsed)) {
+			return ans;
+		}
+	}
+	return NaN;
+};
+
+const solve = (text) => {
+	const lines = text.trim().split('\n');
+	let total = 0;
+	for (let i = 0; i < lines.length; i++) {
+		const t0 = performance.now();
+		const ans = solveLine(lines[i]);
+		const t1 = performance.now();
+		total += ans;
+		console.log(`Line ${i}:`, ans);
+	}
+	console.log(total);
+};
+
+const inputText = fs.readFileSync('./input.txt', 'ascii');
+solve(inputText);
